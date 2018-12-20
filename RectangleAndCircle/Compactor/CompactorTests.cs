288bd7dbd207167ae8bf58fb932b7b2fe02b3e68
amulgachev <amulgachev@ptsecurity.com>
2018-12-20 10:55:21 +0300

    добавлены тесты

--- RectangleAndCircle/Compactor/CompactorTests.cs
+++ RectangleAndCircle/Compactor/CompactorTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using RectangleAndCircle.Rectangle;
+using RectangleAndCircle.RectangleGenerator;
 
 namespace RectangleAndCircle.Compactor
 {
@@ -13,6 +15,19 @@ namespace RectangleAndCircle.Compactor
         {
             Action action = () => CompactorFactory.Create(3, 0);
             action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ShouldRadiusEqual5WhenRectangleGeneratorRadiusEqual5()
+        {
+            var radius = 5;
+            var addAttemptCount = 1;
+            var rectangleGeneratorMock = new Mock<IRectangleGenerator>();
+            rectangleGeneratorMock.Setup(g => g.Radius).Returns(() => radius);
+
+            var compactor = new Compactor(addAttemptCount, rectangleGeneratorMock.Object);
+
+            compactor.Radius.Should().Be(radius);
         }
 
         [Test]
@@ -36,6 +51,21 @@ namespace RectangleAndCircle.Compactor
             rec.Y.Should().Be(height / 2);
             rec.Width.Should().Be(width);
             rec.Height.Should().Be(height);
+        }
+
+        [Test]
+        public void ShouldFalseWhenGenerateSomeRectangles()
+        {
+            var addAttemptCount = 5;
+            var rectangle = new RectangleD(1, 1, 1, 1);
+            var rectangleGeneratorMock = new Mock<IRectangleGenerator>();
+            rectangleGeneratorMock.Setup(g => g.GenerateRectangleInCircle(It.IsAny<RectangleParams>())).Returns(() => rectangle);
+            var compactor = new Compactor(addAttemptCount, rectangleGeneratorMock.Object);
+            compactor.AddRectangle(new RectangleParams(1, 1));
+
+            var isAdded = compactor.AddRectangle(new RectangleParams(1, 1));
+
+            isAdded.Should().BeFalse();
         }
     }
 }
